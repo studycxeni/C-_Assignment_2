667e2ad8b5266132ee67642a652be537c6852c21
studycxeni <123119829+studycxeni@users.noreply.github.com>
2023-03-30 10:45:20 +0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_2 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            int age;
+            string name, lastName, departmentName, position, hospitalName, experience;
+            name = textBox1.Text.ToString();
+            lastName = textBox2.Text.ToString();
+            age = int.Parse(textBox3.Text);
+            departmentName = textBox4.Text.ToString();
+            position = textBox5.Text.ToString();
+            hospitalName = textBox6.Text.ToString();
+            experience = textBox7.Text.ToString();
+
+            Doctor obj_doctor = new Doctor(name, lastName, age, departmentName, position, hospitalName, experience);
+            obj_doctor.Print(label1);
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_2 {
+    class Person {
+        protected string name, lastName;
+        protected int age;
+
+        public Person (string name, string lastName, int age) {
+            this.name = name;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+    }
+    class Doctor : Person {
+        string departmentName, position, hospitalName, experience;
+        public Doctor (string name, string lastName, int age, string departmentName, string position, string hospitalName, string experience) : base (name, lastName, age) {
+            this.departmentName = departmentName;
+            this.position = position;
+            this.hospitalName = hospitalName;
+            this.experience = experience;
+        }
+        public void Print(Label label) {
+            label.Text = String.Format("სახელი : {0} \n გვარი : {1} \n ასაკი : {2} \n განყოფილების დასახელება : {3} \n თანამდებობა : {4} \n" +
+                "საავადმყოფოს დასახელება : {5} \n სტაჟი : {6} \n "
+                , name, lastName, age, departmentName, position, hospitalName, experience);
+        }
+    }
+}
